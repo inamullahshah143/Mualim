0c57d53bb63060918230f8a788fc1c5f6d120875
Inamullah Shah <89691107+inamullahshah143@users.noreply.github.com>
2022-09-26 20:14:43 +0500

    .

--- mualim/lib/controllers/quiz_controller.dart
+++ mualim/lib/controllers/quiz_controller.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use
+
 import 'dart:convert';
 
 import 'package:dio/dio.dart';
@@ -6,7 +8,8 @@ import 'package:get/get.dart';
 import 'package:mualim/model/quiz_model.dart';
 import 'package:mualim/utils/api_utils.dart';
 
-class QuizController extends GetxController {
+class QuizController extends GetxController with SingleGetTickerProviderMixin {
+  List sample_data = [];
   Future<QuizModel?> registrationProcess(int chapterId, context) async {
     try {
       final response = await Dio().post(
@@ -17,6 +20,9 @@ class QuizController extends GetxController {
         data: {'chapter_id': chapterId},
       );
       if (response.statusCode == 200) {
+        for (var element in response.data['quiz']) {
+          sample_data.add(Quiz(id: element[''], question: element[''], details: element[''], chapterId: element[''], options: element[''], correctIndex: element['']));
+        }
         return quizModelFromJson(jsonEncode(response.data));
       } else {
         return null;
@@ -32,101 +38,80 @@ class QuizController extends GetxController {
     }
   }
 
-  AnimationController _animationController;
-  Animation _animation;
-  // so that we can access our animation outside
-  Animation get animation => this._animation;
+  AnimationController? _animationController;
+  Animation? _animation;
+  Animation get animation => _animation!;
 
-  PageController _pageController;
-  PageController get pageController => this._pageController;
-
-  List<QuizModel> _questions = sample_data
+  PageController? _pageController;
+  PageController get pageController => _pageController!;
+  final List<Quiz> _quiz = sample_data
       .map(
-        (question) => QuizModel(
-            id: question['id'],
-            question: question['question'],
-            options: question['options'],
-            answer: question['answer_index']),
+        (quiz) => Quiz(
+          chapterId: quiz['chapter_id'],
+          correctIndex: quiz['correctIndex'],
+          details: quiz['details'],
+          id: quiz['id'],
+          options: quiz['options'],
+          question: quiz['question'],
+        ),
       )
       .toList();
-  List<QuizModel> get questions => this._questions;
+  List<Quiz> get quiz => _quiz;
 
   bool _isAnswered = false;
-  bool get isAnswered => this._isAnswered;
+  bool get isAnswered => _isAnswered;
 
-  int _correctAns;
-  int get correctAns => this._correctAns;
+  int? _correctAns;
+  int get correctAns => _correctAns!;
 
-  int _selectedAns;
-  int get selectedAns => this._selectedAns;
-
-  // for more about obs please check documentation
-  RxInt _questionNumber = 1.obs;
-  RxInt get questionNumber => this._questionNumber;
+  int? _selectedAns;
+  int get selectedAns => _selectedAns!;
+  final RxInt _questionNumber = 1.obs;
+  RxInt get questionNumber => _questionNumber;
 
   int _numOfCorrectAns = 0;
-  int get numOfCorrectAns => this._numOfCorrectAns;
-
-  // called immediately after the widget is allocated memory
+  int get numOfCorrectAns => _numOfCorrectAns;
   @override
   void onInit() {
-    // Our animation duration is 60 s
-    // so our plan is to fill the progress bar within 60s
-    _animationController = AnimationController(duration: Duration(seconds: 60), vsync: this);
-    _animation = Tween<double>(begin: 0, end: 1).animate(_animationController)
+    _animationController =
+        AnimationController(duration: const Duration(seconds: 60), vsync: this);
+    _animation = Tween<double>(begin: 0, end: 1).animate(_animationController!)
       ..addListener(() {
-        // update like setState
         update();
       });
-
-    // start our animation
-    // Once 60s is completed go to the next qn
-    _animationController.forward().whenComplete(nextQuestion);
+    _animationController!.forward().whenComplete(nextQuestion);
     _pageController = PageController();
     super.onInit();
   }
-
-  // // called just before the Controller is deleted from memory
   @override
   void onClose() {
     super.onClose();
-    _animationController.dispose();
-    _pageController.dispose();
+    _animationController!.dispose();
+    _pageController!.dispose();
   }
 
-  void checkAns(Question question, int selectedIndex) {
-    // because once user press any option then it will run
+  void checkAns(Quiz quiz, int selectedIndex) {
     _isAnswered = true;
-    _correctAns = question.answer;
+    _correctAns = quiz.correctIndex;
     _selectedAns = selectedIndex;
 
     if (_correctAns == _selectedAns) _numOfCorrectAns++;
-
-    // It will stop the counter
-    _animationController.stop();
+    _animationController!.stop();
     update();
-
-    // Once user select an ans after 3s it will go to the next qn
-    Future.delayed(Duration(seconds: 3), () {
+    Future.delayed(const Duration(seconds: 3), () {
       nextQuestion();
     });
   }
 
   void nextQuestion() {
-    if (_questionNumber.value != _questions.length) {
+    if (_questionNumber.value != _quiz.length) {
       _isAnswered = false;
-      _pageController.nextPage(
-          duration: Duration(milliseconds: 250), curve: Curves.ease);
-
-      // Reset the counter
-      _animationController.reset();
-
-      // Then start it again
-      // Once timer is finish go to the next qn
-      _animationController.forward().whenComplete(nextQuestion);
+      _pageController!.nextPage(
+          duration: const Duration(milliseconds: 250), curve: Curves.ease);
+      _animationController!.reset();
+      _animationController!.forward().whenComplete(nextQuestion);
     } else {
-      // Get package provide us simple way to navigate another page
-      Get.to(ScoreScreen());
+      // Get.to(ScoreScreen());
     }
   }
 
